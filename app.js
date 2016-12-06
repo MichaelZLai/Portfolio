2657fc3ee065607cf8283816826b7f97b6061839
Michael Lai <mike24567@gmail.com>
2016-12-06 15:29:24 -0500

    figured out the navbar

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+//Scroll Navigation feature
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 520) {
+    $("nav").addClass("showNav");
+  } else {
+    $("nav").removeClass("showNav");
+  }
+});
