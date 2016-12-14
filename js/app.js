daa51bf1b0ce526280fd5ae4f3f08f6c4d374452
Michael Lai <mike24567@gmail.com>
2016-12-14 09:16:26 -0500

    cleaned up js

--- js/app.js
+++ js/app.js
@@ -13,14 +13,14 @@ if($(window).width() > 736){
   $("nav .navbar .showNav").show();
 }
 
-//have landing page at the top
+//Have landing page at the top
 
 $(document).ready(function(){
     $(this).scrollTop(0);
 });
 
 
-
+//Preloader functionality
 $(window).on('load', function() { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
