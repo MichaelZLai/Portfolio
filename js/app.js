705093b5af3b4d47d7092aa392f3fae569178f6c
Michael Lai <mike24567@gmail.com>
2016-12-13 20:03:45 -0500

    added loading screen

--- js/app.js
+++ js/app.js
@@ -18,3 +18,11 @@ if($(window).width() > 736){
 $(document).ready(function(){
     $(this).scrollTop(0);
 });
+
+
+
+$(window).on('load', function() { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({'overflow':'visible'});
+})
