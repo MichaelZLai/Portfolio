d7891c82abfca32ff594a9066d954bd57866d615
Michael Lai <mike24567@gmail.com>
2019-02-05 13:21:20 -0800

    revising preloader and adding resume

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
+$(window).on('load', _ => {
 
     setTimeout(function(){
         $('body').addClass('loaded');
-    }, 3000);
+    }, 1000);
 
 });
+
+
+//Preloader functionality
+// $(window).on('load', _ => { // makes sure the whole site is loaded
+//     $('#status').fadeOut(); // will first fade out the loading animation
+//     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+//     $('body').delay(350).css({'overflow':'visible'});
+// })
