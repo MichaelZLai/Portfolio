331a331247a8b369b0892fa466b52d4f8f31a2d1
Michael Lai <mike24567@gmail.com>
2019-02-05 10:29:53 -0800

    fixed side nav issue

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -4,9 +4,9 @@
   // Closes the sidebar menu
   $(".menu-toggle").click(function(e) {
     e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
+    $("#sidebar-wrapper").toggleClass("active-s");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    $(this).toggleClass("active");
+    $(this).toggleClass("active-s");
   });
 
   // Smooth scrolling using jQuery easing
@@ -25,8 +25,8 @@
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('#sidebar-wrapper .js-scroll-trigger').click(function() {
-    $("#sidebar-wrapper").removeClass("active");
-    $(".menu-toggle").removeClass("active");
+    $("#sidebar-wrapper").removeClass("active-s");
+    $(".menu-toggle").removeClass("active-s");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
   });
 
@@ -41,24 +41,3 @@
   });
 
 })(jQuery); // End of use strict
-
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
